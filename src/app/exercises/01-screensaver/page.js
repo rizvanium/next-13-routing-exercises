8e9d784e4caf3c53b368a4755641d1f1b92aa4d0
Rizvan Chalilovas <rizvanium@Rizvans-MBP.lan>
2024-02-03 20:51:45 +0200

    finish ex.: 1

--- src/app/exercises/01-screensaver/page.js
+++ src/app/exercises/01-screensaver/page.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import Link from 'next/link'
 
-import ScreenSaver from '../../../components/ScreenSaver';
-
-function ScreenSaverExercise() {
+function ScreenSaverIndex() {
   return (
-    <main className="screen-saver-wrapper">
-      <ScreenSaver color="red" />
+    <main>
+      <p>Choose your color:</p>
+      <ul>
+        <li><Link href="/exercises/01-screensaver/lavender">lavender</Link></li>
+        <li><Link href="/exercises/01-screensaver/slateblue">slateblue</Link></li>
+        <li><Link href="/exercises/01-screensaver/hotpink">hotpink</Link></li>
+        <li><Link href="/exercises/01-screensaver/white">white</Link></li>
+      </ul>
     </main>
   );
 }
 
-export default ScreenSaverExercise;
+export default ScreenSaverIndex;
